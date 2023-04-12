f7f9cbbcdd291f7bc108e8a64142b6e3ae06b4a8
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2023-04-12 12:29:39 +1000

    Added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,6 @@
 setInterval(function () {
 let budapestElement = document.querySelector("#budapest");
+if (budapestElement) {
 let budapestDateElement = budapestElement.querySelector(".date");
 let budapestTimeElement = budapestElement.querySelector(".time");
 let budapestTime = moment().tz("Europe/Budapest");
@@ -7,17 +8,40 @@ let budapestTime = moment().tz("Europe/Budapest");
 
 budapestDateElement.innerHTML = budapestTime.format("MMMM Do YYYY");
 budapestTimeElement.innerHTML = `${budapestTime.format("h:mm:ss")} <small>${budapestTime.format("A")}</small>`;
-}, 1000);   
+}}, 1000);   
+
+
 
 function updateSanPauloTime () {
 let saoPauloElement = document.querySelector("#sao-paulo");
+if (saoPauloElement) {
 let saoPauloDateElement = saoPauloElement.querySelector(".date");
 let saoPauloTimeElement = saoPauloElement.querySelector(".time");
 let saoPauloTime = moment().tz("America/Sao_Paulo");
 
 saoPauloDateElement.innerHTML = saoPauloTime.format("MMMM Do YYYY");
-saoPauloTimeElement.innerHTML = saoPauloTime.format("h:mm:ss  [<small>]A[</small>]");
+saoPauloTimeElement.innerHTML = saoPauloTime.format("h:mm:ss [<small>]A[</h:mm:sssmall>]");
+}
 }
 
 updateSanPauloTime();
 setInterval(updateSanPauloTime, 1000);
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", "").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `  
+    <div class="city">
+        <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
+
